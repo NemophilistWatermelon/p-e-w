f22e62ad3e6cb50c16910bc708c6f8f727dbdc6a
WaterMelon <937817514@qq.com>
2022-06-18 21:21:22 +0800

    feat: word show

--- pwe-app/src/views/HomeView.vue
+++ pwe-app/src/views/HomeView.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="file" @input="onInputFileAction" />
+    <div id="container" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { defaultOptions, renderAsync } from 'docx-preview'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      vHtml: '',
+      wordURL: '' //文件地址
+    }
+  },
+  methods: {
+    onInputFileAction(event) {
+      const file = event.target.files[0]
+      const fileReader = new FileReader()
+      fileReader.readAsArrayBuffer(file, 'utf-8')
+      fileReader.onload = function (args) {
+        console.log(args.target.result)
+        renderAsync(
+          args.target.result,
+          document.getElementById('container'),
+          null,
+          {
+            className: 'docx', // 默认和文档样式类的类名/前缀
+            inWrapper: true, // 启用围绕文档内容渲染包装器
+            ignoreWidth: false, // 禁止页面渲染宽度
+            ignoreHeight: false, // 禁止页面渲染高度
+            ignoreFonts: false, // 禁止字体渲染
+            breakPages: true, // 在分页符上启用分页
+            ignoreLastRenderedPageBreak: true, //禁用lastRenderedPageBreak元素的分页
+            experimental: true, //启用实验性功能（制表符停止计算）
+            trimXmlDeclaration: true, //如果为真，xml声明将在解析之前从xml文档中删除
+            debug: false // 启用额外的日志记录
+          }
+        )
+      }
+    }
   }
 }
 </script>
--- pwe-app/vue.config.js
+++ pwe-app/vue.config.js
@@ -1,7 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
+
+
 module.exports = defineConfig({
+  lintOnSave: false,
   transpileDependencies: true,
   devServer: {
     open: true
+  },
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        path: require.resolve('path-browserify'),
+        stream: require.resolve("stream-browserify")
+      }
+      
+    }
   }
 })
