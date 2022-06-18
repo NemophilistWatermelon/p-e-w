e79f5bf112ba3ef58791fd811b873f23bd4d6c33
WaterMelon <937817514@qq.com>
2022-06-18 22:48:45 +0800

    feat: excel show

--- pwe-app/src/views/HomeView.vue
+++ pwe-app/src/views/HomeView.vue
@@ -1,23 +1,38 @@
 <template>
   <div class="home">
-    <input type="file" @input="onInputFileAction" />
+    <h1>word 预览</h1>
+    <input type="file" @input="onChangeWordUpload" />
     <div id="container" />
+
+    <h2>excel</h2>
+    <input type="file" @input="onChangeExcelUpload" />
+    <div>keys: {{ excelKeys }}</div>
+    <div>{{ excelData }}</div>
   </div>
 </template>
 
 <script>
+// word 文档预览插件
 import { defaultOptions, renderAsync } from 'docx-preview'
-
+// excel 解析插件
+import xlsx from 'xlsx'
+import { ref } from 'vue'
 export default {
   name: 'HomeView',
   data() {
     return {
       vHtml: '',
-      wordURL: '' //文件地址
+      wordURL: '', //文件地址,
+      excelData: {},
+      excelKeys: []
     }
   },
+  setup() {
+    var abc = ref()
+    console.log(abc)
+  },
   methods: {
-    onInputFileAction(event) {
+    onChangeWordUpload(event) {
       const file = event.target.files[0]
       const fileReader = new FileReader()
       fileReader.readAsArrayBuffer(file, 'utf-8')
@@ -40,6 +55,29 @@ export default {
             debug: false // 启用额外的日志记录
           }
         )
+      }
+    },
+
+    onChangeExcelUpload(event) {
+      const file = event.target.files[0]
+      const fileReader = new FileReader()
+      // readAsArrayBuffer 方法将文件内容读取到ArrayBuffer对象中
+      // readAsBinaryString 方法将文件内容读取到二进制字符串中
+      fileReader.readAsBinaryString(file, 'utf-8')
+      var me = this
+      console.log(me)
+      fileReader.onload = function (args) {
+        console.log(args.target.result)
+        var data = args.target.result
+        console.log(data.length)
+        // 根据读的调用方法传递type 配置类型
+        var workbook = xlsx.read(data, { type: 'binary' })
+        var sheetNames = workbook.SheetNames // 工作表名称集合
+        var worksheet = workbook.Sheets[sheetNames[0]]
+        var excelData = xlsx.utils.sheet_to_json(worksheet)
+        console.log(excelData) //最终解析xlsx格式的文件得出来的数据
+        me.excelKeys = Object.keys(excelData[0])
+        me.excelData = JSON.parse(JSON.stringify(excelData, null, 4))
       }
     }
   }
